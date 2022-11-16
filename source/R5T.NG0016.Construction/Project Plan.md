ba604d27d699444ac075575420dbe7a013a2964a
davidcoats <david@getsafetycone.com>
2022-11-16 09:30:38 -0800

    Initial commit.

---
+++ source/R5T.NG0016.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.NG0016.Construction
+Construction console project for the R5T.NG0016 library.
---
+++ source/R5T.NG0016/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.NG0016
+{
+	/// <summary>
+	/// BlazorTemplater NuGet package selector.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
